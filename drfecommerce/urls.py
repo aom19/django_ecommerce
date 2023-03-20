ba47d0ae6dd3a4b80f196c86e0a21a1bb0ac4923
aom19 <39848284+aom19@users.noreply.github.com>
2023-03-20 20:17:01 +0200

    reformat the urls and moved category to apiview

--- drfecommerce/urls.py
+++ drfecommerce/urls.py
@@ -7,18 +7,14 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from drfecommerce.product import views
 
 
-router = routers.DefaultRouter()
-router.register(r'brand', views.BrandViewSet)
-router.register(r'category', views.CategoryViewSet)
-router.register(r'product', views.ProductViewSet)
-
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls) , name='api'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/', include('drfecommerce.product.urls')),
+
 
 
 ]
